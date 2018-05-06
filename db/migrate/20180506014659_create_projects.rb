2fd72ded5312a835e034e48b6782b146ed66d112
Darrow87 <alexforgerii@gmail.com>
2018-05-06 16:19:24 -0400

    add rows to user and projects table

--- db/migrate/20180506014659_create_projects.rb
+++ db/migrate/20180506014659_create_projects.rb
@@ -1,7 +1,12 @@
 class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
-      t.string :name
+      t.string :description
+      t.string :city
+      t.string :state
+      t.string :genre
+      t.string :equipment
+      t.string :needed_equipment
 
       t.timestamps null: false
     end
