86a92c4cb4f1440d73881600a92fc485cd870d0f
Darrow87 <alexforgerii@gmail.com>
2018-05-06 16:36:30 -0400

    make welcome controller the root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'user/show'
 
   get 'welcome/index'
+  root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
