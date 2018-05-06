5e49eb669196b3e70c6be6c88d8e89fb9ca68113
Darrow87 <alexforgerii@gmail.com>
2018-05-06 10:53:05 -0400

    add projects users join table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180506043722) do
+ActiveRecord::Schema.define(version: 20180506144906) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "text"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20180506043722) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "projects_users", id: false, force: :cascade do |t|
+    t.integer "project_id", null: false
+    t.integer "user_id",    null: false
+  end
+
+  add_index "projects_users", ["project_id"], name: "index_projects_users_on_project_id"
+  add_index "projects_users", ["user_id"], name: "index_projects_users_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
