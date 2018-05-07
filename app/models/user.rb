7a32a81dfcbc3d2d23fd83df754a2bec412233ca
Darrow87 <alexforgerii@gmail.com>
2018-05-07 14:26:36 -0400

    fix bug in user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates :name, :email, :password, :presence: true
+  validates :name, :email, :password, presence: true
   validates :password, :length => {:within => 6..40}
   has_secure_password
   has_and_belongs_to_many :projects
