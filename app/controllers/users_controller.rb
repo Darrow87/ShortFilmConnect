9c5339ac43b379ec0756c135d9d88789254bc429
Darrow87 <alexforgerii@gmail.com>
2018-05-07 12:45:36 -0400

    add user create in user controller and strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,14 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.create(params[user_params])
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
+
 end
