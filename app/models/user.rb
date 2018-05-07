938f0591761c7e6e81f5a2a066c5190152f2b362
Darrow87 <alexforgerii@gmail.com>
2018-05-07 13:17:42 -0400

    add user name email and password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  validates :name, :email, :password, :presence: true
+  validates :password, :length => {:within => 6..40}
   has_secure_password
   has_and_belongs_to_many :projects
 end
