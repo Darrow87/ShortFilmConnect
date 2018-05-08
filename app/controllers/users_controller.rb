514ed409b2ce82243bf131a4da404c30b11da327
Darrow87 <alexforgerii@gmail.com>
2018-05-08 17:09:32 -0400

    add logged in user method to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,4 +40,11 @@ before_action :logged_in_user, only: [:edit, :update]
       params.require(:user).permit(:name, :email, :password)
     end
 
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
 end
