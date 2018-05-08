40d77b75b7cc475641cc4457bf483df1add7efdd
Darrow87 <alexforgerii@gmail.com>
2018-05-08 10:28:06 -0400

    refactor current user method again

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,5 +3,5 @@ module SessionsHelper
     sessions[:user_id] = user.id
   end
 
-  @current_user = @current_user || User.find_by(id: session[:user_id])
+  @current_user ||= User.find_by(id: session[:user_id])
 end
