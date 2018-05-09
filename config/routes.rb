9311895295843d2bdc2a750e9c6477b29a2f7802
Darrow87 <alexforgerii@gmail.com>
2018-05-09 11:23:47 -0400

    add static pages routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  get  'static_pages/home'
+  get  'static_pages/help'
+
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
   resources :users, except: [:index]
-
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
